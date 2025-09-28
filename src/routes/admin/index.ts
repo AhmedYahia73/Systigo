70c930569959e6bb52f274620dda4d4b6593a763
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-09-28 16:57:37 +0300

    add all routes in admin in main routes

--- src/routes/admin/index.ts
+++ src/routes/admin/index.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
 import authRouter from "./auth";
+import CouponRouter from "./CouponRoutes";
+import PackageRouter from "./PackageRoutes";
+import ClientRoutes from "./clientRoutes";
+import PaymentMethodRoutes from "./paymentMethodRoutes";
+import ThemeRoutes from "./themesRoutes";
+import router from "./clientRoutes";
 //import { authenticated } from "../../middlewares/authenticated";
 //import {  authorizeRoles } from "../../middlewares/authorized";
 
@@ -8,5 +14,10 @@ export const route = Router();
 route.use("/auth", authRouter);
 // route.use(authenticated,authorizeRoles("admin"));
 
+route.use("/coupons", CouponRouter);
+route.use("/packages", PackageRouter);
+route.use("/clients", ClientRoutes);
+route.use("/payment-methods", PaymentMethodRoutes);
+route.use("/themes", ThemeRoutes);
 
 export default route;
