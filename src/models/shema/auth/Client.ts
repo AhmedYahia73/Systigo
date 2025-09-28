dd1531f51e4bca7ad7a27f871dc176edec77c1af
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-09-28 16:56:08 +0300

    Update Client.ts

--- src/models/shema/auth/Client.ts
+++ src/models/shema/auth/Client.ts
@@ -1,19 +1,24 @@
 import { model, Schema, Types } from "mongoose";
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
+
+
  const ClientSchema = new Schema(
   {
     
-    name: { type: String, },
+    company_name: { type: String, },
     email: { type: String, unique: true },
     password: { type: String},
-    BaseImage64: { type: String},
-    fcmtoken: { type: String },
-    isVerified: { type: Boolean, default: false },
-    googleId: { type: String, unique: true, sparse: true },   
-
+    status: {type: String},
+    package_id: { type: Types.ObjectId, ref: 'Package' },
   },
   { timestamps: true, }
 );
 
+ClientSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 export const ClientModel = mongoose.model('Client', ClientSchema);
 
