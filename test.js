4a86d2bf707ca30dda6ae1815da0fbdf78247c8b
&mazen <mazenkhairy200@gmail.com>
2026-02-14 09:56:37 +0200

    test

--- test.js
+++
@@ -1,8 +0,0 @@
-import bcrypt from "bcrypt";
-
-const plainPassword = "12345678";
-
-// Hashing
-const hashedPassword = await bcrypt.hash(plainPassword, 10); // 10 is the salt rounds
-
-console.log("Hashed password:", hashedPassword);
